d5417535c6633ff5b86e011b7608ad8ad1a9d70a
redniche <redniche@naver.com>
2022-02-12 17:50:22 +0900

    Update share_proof.js
    updated
    - delete socket line

--- HTGAWM/Assets/WebGLTemplates/ProjectTemplate/share_proof.js
+++ HTGAWM/Assets/WebGLTemplates/ProjectTemplate/share_proof.js
@@ -1,14 +1,13 @@
-var socket = io() || {};
-socket.isReady = false;
+/** @format */
 
 // chatroom client .js
 window.addEventListener("load", function () {
-  // 탐색 후 1차 회의 시간
-  socket.on("SHARE_PROOF", function (_data) {
-    if (window.unityInstance != null) {
-      console.log(window.unityInstance);
-      console.log("공유받은 증거 Unity로 전송");
-      window.unityInstance.SendMessage("Player", "ReceiveSharedProof", _data);
-    }
-  }); //END_SOCKET.ON
+	// 탐색 후 1차 회의 시간
+	socket.on("SHARE_PROOF", function (_data) {
+		if (window.unityInstance != null) {
+			console.log(window.unityInstance);
+			console.log("공유받은 증거 Unity로 전송");
+			window.unityInstance.SendMessage("Player", "ReceiveSharedProof", _data);
+		}
+	}); //END_SOCKET.ON
 }); //END_window_addEventListener
